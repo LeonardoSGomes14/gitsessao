643d0cea864821e67b125875d85b83a905b2f768
LeonardoSGomes14 <leoleonardo131099@gmail.com>
2023-10-16 10:56:19 -0300

    commit: criação da tela de login

--- login.php
+++ login.php
@@ -1,17 +1,11 @@
-
 <!DOCTYPE html>
 <html lang="pt-BR">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Sessão</title>
+    <title>tela de login</title>
 </head>
 <body>
     
-<h1>TESTE</h1>
-
 </body>
 </html>
-
-
-
